9794d622b6b1f48d02a92934e8e54edc64b6fdc8
edgardfreitas <edgardfp@gmail.com>
2025-03-21 11:11:05 -0300

    base url added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -12,7 +12,7 @@ describe('Orange HRM tests', () => {
   }
 
   it('Login - Success', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
