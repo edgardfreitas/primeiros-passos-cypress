07fbeee5762153ec18d75e9e6f27f1c26e833276
edgardfreitas <edgardfp@gmail.com>
2025-03-24 11:11:33 -0300

    add random values on user spec chancejs

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -2,26 +2,19 @@ import userData from '../fixtures/users/userData.json'
 import LoginPage from '../pages/loginPage'
 import DashboardPage from '../pages/dashboardPage'
 import MenuPage from '../pages/menuPage'
+import MyInfoPage from '../pages/myInfoPage'
 
+const Chance = require('chance')
+
+const chance = new Chance()
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const menuPage = new MenuPage()
+const myInfoPage = new MyInfoPage()
 
 describe('Orange HRM tests', () => {
 
-  const selectorsList = {
-    firstNameField: "[name='firstName']",
-    lastNameField: "[name='lastName']",
-    genericField: ".oxd-input--active",
-    dateField: "[placeholder='yyyy-dd-mm']",
-    genericComboBox: ".oxd-select-text--arrow",
-    secondItemCombobox: ".oxd-select-dropdown > :nth-child(2)",
-    thirdItemCombobox: ".oxd-select-dropdown > :nth-child(3)",
-    dateCloseButton: ".--close",
-    submitButton: "[type='submit']" 
-  }
-
-  it.only('User Info Update - Success', () => {
+  it('User Info Update - Success', () => {
 
     loginPage.accessLoginPage()
     loginPage.loginWithAnyUser(userData.userSuccess.username, userData.userSuccess.password)
@@ -30,29 +23,11 @@ describe('Orange HRM tests', () => {
   
     menuPage.accessMyInfo()
 
-    cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
-    cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
-    cy.get(selectorsList.genericField).eq(3).clear().type('EmpIdTest')
-    cy.get(selectorsList.genericField).eq(4).clear().type('OtherIdTest')
-    cy.get(selectorsList.genericField).eq(5).clear().type('DriverLicenseTest')
-    cy.get(selectorsList.dateField).eq(0).clear().type('2025-25-12')
-    cy.get(selectorsList.dateCloseButton).click()
+    myInfoPage.fillPersonalDetails(chance.first(), chance.last())
+    myInfoPage.fillEmployeeDetails('EmployeeId', 'otherId', '123456', '2025-07-29')
+    myInfoPage.fillStatus()
+    myInfoPage.saveForm()
 
-    cy.get(selectorsList.genericComboBox).eq(0).click({ force: true })
-    cy.get(selectorsList.secondItemCombobox).click()
-    cy.get(selectorsList.genericComboBox).eq(1).click({ force: true })
-    cy.get(selectorsList.thirdItemCombobox).click()
-
-    cy.get(selectorsList.submitButton).eq(0).click({ force: true })
-    cy.get('body').should('contain', 'Successfully Updated')
-    cy.get('.oxd-toast-close')
   })
 
-  it('Login - Fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userFail.username)
-    cy.get(selectorsList.passwordField).type(userData.userFail.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.get(selectorsList.wrongCredentialAlert)
-  })
 })
